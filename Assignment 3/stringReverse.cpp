cab51c605a0185a32876da3fa920530a50a55036
RottenKiwi2004 <61570087+RottenKiwi2004@users.noreply.github.com>
2023-05-16 08:51:28 +0000

    Added Assignment 3

---
+++ Assignment 3/stringReverse.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printArray(char * str, int length) {
+	for(int i=0;i<length;i++) {
+		cout << str[i];
+	}
+	cout << endl;
+}
+
+int main()
+{
+	char str[1000];
+	cout << "Input string: ";
+	cin >> str;
+
+	int length = strlen(str);
+
+	cout << "Original String: ";
+	printArray(str, length);
+	
+	char *pa = &str[0], *pb = &str[length-1];
+	for(int i=0 ; i< length / 2; i++, pa++, pb--) {
+		int temp = *pb; *pb = *pa; *pa = temp;
+	}
+
+	cout << "Reversed String: ";
+	printArray(str, length);
+		
+}
