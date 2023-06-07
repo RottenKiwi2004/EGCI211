d565f4e01b30a91b180dd69ac3dce37c17785793
RottenKiwi <markkittiphat@gmail.com>
2023-06-07 22:28:05 +0700

    Updated bubbleSort

--- SortingAlgo/bubbleSort.cpp
+++ SortingAlgo/bubbleSort.cpp
@@ -17,8 +17,18 @@ int main()
 
 void bubbleSort(vector<int> &v)
 {
-	for (int i = 0; i < v.size(); i++)
-		for (int j = i; j < v.size(); j++)
-			if (v[i] > v[j])
-				swap(v[i], v[j]);
+	for (int i = v.size() - 1; i >= 0; i--)
+	{
+		bool sorted = true;
+		for (int j = 0; j < i; j++)
+		{
+			if (v[j] > v[j + 1])
+			{
+				swap(v[j], v[j + 1]);
+				sorted = false;
+			}
+		}
+		if (sorted)
+			break;
+	}
 }
