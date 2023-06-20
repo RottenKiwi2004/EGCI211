7547e818fa487e68036b6224a8817597825a2f99
RottenKiwi <markkittiphat@gmail.com>
2023-06-20 23:01:23 +0700

    Added setw for minute

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -3,17 +3,18 @@ using namespace std;
 #include <iomanip>
 #include "Time.h"
 
-int main(){
-  int h1,m1, h2,m2,h3,m3;
-  cout<<"What time was it?"<<endl;
-  getTime(h1,m1);
-	// I added only single endl, because it was annoying to see 2 lines are not consistent 
-  cout<<"What time is it now?"<<endl;
-  getTime(h2,m2);
+int main()
+{
+  int h1, m1, h2, m2, h3, m3;
+  cout << "What time was it?" << endl;
+  getTime(h1, m1);
+  // I added only single endl, because it was annoying to see 2 lines are not consistent
+  cout << "What time is it now?" << endl;
+  getTime(h2, m2);
 
-  cout<<"Time diff is ";
-  h3=subtract(h1,m1,h2,m2); 
-  m3=subtract(m1,m2); 
-  cout<<setfill('0')<<setw(2)<<h3<<":";
-  cout<<setfill('0')<<m3<<endl;
+  cout << "Time diff is ";
+  h3 = subtract(h1, m1, h2, m2);
+  m3 = subtract(m1, m2);
+  cout << setfill('0') << setw(2) << h3 << ":";
+  cout << setfill('0') << setw(2) << m3 << endl;
 }
